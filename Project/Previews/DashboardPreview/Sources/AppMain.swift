fa4a1acf74aae94fc8c52654550a4604cfc3b2b0
승찬 <anstmdcks12@naver.com>
2024-07-11 17:39:46 +0900

    feat: Core, Feature 베이스 추가 및 필요한 베이스 패키지들 연동

---
+++ Project/Previews/DashboardPreview/Sources/AppMain.swift
@@ -0,0 +1,18 @@
+import Architecture
+import LinkNavigator
+import SwiftUI
+
+// MARK: - AppMain
+
+struct AppMain {
+  let viewModel: AppViewModel
+}
+
+// MARK: View
+
+extension AppMain: View {
+
+  var body: some View {
+    Text("App")
+  }
+}
