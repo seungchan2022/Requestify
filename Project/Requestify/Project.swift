193371f366e6e8bfdd848fd018f0cef8e51c656c
승찬 <anstmdcks12@naver.com>
2024-07-11 17:12:22 +0900

    feat: Tuist 베이스 설정 파일 추가

---
+++ Project/Requestify/Project.swift
@@ -0,0 +1,72 @@
+import ProjectDescription
+import ProjectDescriptionHelpers
+
+let targetList: [Target] = [
+  .target(
+    name: "Requestify-Production",
+    destinations: .iOS,
+    product: .app,
+    productName: "Requestify",
+    bundleId: "io.seungchan.requestify",
+    deploymentTargets: .default,
+    infoPlist: .defaultInfoPlist,
+    sources: ["Sources/**"],
+    resources: ["Resources/**"],
+    copyFiles: .none,
+    headers: .none,
+    entitlements: .none,
+    scripts: [],
+    dependencies: .default,
+    settings: .defaultConfig(false),
+    coreDataModels: [],
+    environmentVariables: [:],
+    launchArguments: [],
+    additionalFiles: [],
+    buildRules: [],
+    mergedBinaryType: .disabled,
+    mergeable: false),
+
+  .target(
+    name: "Requestify-QA",
+    destinations: .iOS,
+    product: .app,
+    productName: "Requestify",
+    bundleId: "io.seungchan.requestify",
+    deploymentTargets: .iOS("17.0"),
+    infoPlist: .defaultInfoPlist,
+    sources: ["Sources/**"],
+    resources: ["Resources/**"],
+    copyFiles: .none,
+    headers: .none,
+    entitlements: .none,
+    scripts: [],
+    dependencies: .default,
+    settings: .defaultConfig(true),
+    coreDataModels: [],
+    environmentVariables: [:],
+    launchArguments: [],
+    additionalFiles: [],
+    buildRules: [],
+    mergedBinaryType: .disabled,
+    mergeable: false),
+]
+
+let project: Project = .init(
+  name: "GroomApplication",
+  organizationName: "SeungChanMoon",
+  options: .options(),
+  packages: [],
+  settings: .settings(),
+  targets: targetList,
+  schemes: [],
+  fileHeaderTemplate: .none,
+  additionalFiles: [],
+  resourceSynthesizers: [])
+
+extension [TargetDependency] {
+  public static var `default`: [TargetDependency] {
+    [
+//      .package(product: "Dashboard", type: .runtime),
+    ]
+  }
+}
